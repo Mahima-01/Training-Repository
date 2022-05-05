560bf4c009c187d2cecd9fe6f1c12f5e3cbf7917
Mahima-01 <102168844+Mahima-01@users.noreply.github.com>
2022-05-05 19:46:26 +0530

    Primenumber_Program

---
+++ Ruby/Programs/Prime_number.rb
@@ -0,0 +1,29 @@
+3.Ruby code to check whether a number is prime or not:
+puts 'Enter the number:'
+num = gets.chomp.to_i
+count = 0
+if num.zero?
+  puts '0 is not prime'
+else
+  i = 2
+  while i < num
+    (num % i).zero?
+    count += 1
+    i += 1
+  end
+end
+if count > 1
+  puts "#{num} is not a prime number"
+else
+  puts "#{num} is a prime number"
+end
+
+OUTPUT:
+itsacheckmate@itsacheckmate:~/Training-Repository/Rvm and Ruby$ ruby Ruby.rb
+Enter the number:
+25
+25 is a prime number
+itsacheckmate@itsacheckmate:~/Training-Repository/Rvm and Ruby$ ruby Ruby.rb
+Enter the number:
+40
+40 is not a prime number

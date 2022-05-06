56c3be3ad9c01e14df26a532a8a191286da3b74e
Mahima-01 <102168844+Mahima-01@users.noreply.github.com>
2022-05-06 13:04:58 +0530

    Program to find ASCII value in ruby.

---
+++ Ruby/Programs/ASCII.rb
@@ -0,0 +1,19 @@
+Ruby program to print character of given ASCII value:
+puts 'Enter ASCII number:'
+num = gets.chomp.to_i
+
+puts 'Character is:', num.chr
+
+OUTPUT:
+itsacheckmate@itsacheckmate:~/Training-Repository/Rvm and Ruby$ ruby Ruby.rb
+Enter ASCII number:
+65
+Character is:
+A
+
+itsacheckmate@itsacheckmate:~/Training-Repository/Rvm and Ruby$ ruby Ruby.rb
+Enter ASCII number:
+126
+Character is:
+~
+

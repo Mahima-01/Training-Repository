6b21c3169923326145cd5be3593ffee005ce0fb7
Mahima-01 <102168844+Mahima-01@users.noreply.github.com>
2022-05-13 14:08:53 +0530

    Deleting q 45 as I am making some changes in code.

--- Ruby/Programs/q 45.rb
+++
@@ -1,9 +0,0 @@
-# 45.Given a range of years as a string, return the number of leap years there are within the range (inclusive).
-require 'rspec'
-require 'date'
-
-years = (1900..2000).to_a
-leap_years = []
-years.each do |year|
-  leap_years << year if year % 4.zero?
-end

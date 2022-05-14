de0e15e9a709125e250c3a2242efd19c3bd220ec
Mahima-01 <102168844+Mahima-01@users.noreply.github.com>
2022-05-14 17:50:38 +0530

    Delete q9 as some changes is to be done.

--- Ruby/Programs/q9.rb
+++
@@ -1,5 +0,0 @@
-# 9. Given a string s, find the first non-repeating character in it and return its index. If it does not exist, return -1.
-def first_non_repeat_character(string)
-  string.chars.find { |character| string.count(character) == 1 }
-end
-  first_non_repeat_character('teststring') # => "e"

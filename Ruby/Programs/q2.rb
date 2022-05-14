df399cb15d3c0af7fa8eee631538f85cd5f1ff20
Mahima-01 <102168844+Mahima-01@users.noreply.github.com>
2022-05-14 14:25:47 +0530

    String assignment q2- Write a function to return reverse of a string using recursion.

---
+++ Ruby/Programs/q2.rb
@@ -0,0 +1,15 @@
+2. Write a function to return reverse of a string using recursion.
+def reverse_recursion (string)
+  if string.length <= 1 then return string end
+
+  last = string[-1]
+  string = string[0...-1]
+  string = reverse_recursion(string)
+  last + string 
+end
+
+string = "hello reverse"
+print reverse_recursion(string) , "\n"
+
+itsacheckmate@itsacheckmate:~/Training-Repository/Rvm_Ruby$ ruby Ruby.rb
+esrever olleh

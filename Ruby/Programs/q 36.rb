b0434ac881cd54d4b50d1abc5caea598b9780379
Mahima-01 <102168844+Mahima-01@users.noreply.github.com>
2022-05-16 00:00:05 +0530

    Hash assignment q36 - Return the maximum number of instances  that can be formed.

---
+++ Ruby/Programs/q 36.rb
@@ -0,0 +1,22 @@
+36. Given a string text, you want to use the characters of text to form as many instances of the word "balloon" as possible. You can use each character in text at most once. Return the maximum number of instances that can be formed. 
+# Example:   loonballxballpoon  
+#You can create only 2 balloon from example text, so return 2 
+
+module Baloon_charachter_occurence
+  def self.no_of_ballon(text)
+    hash = Hash.new
+    text.chars.uniq.each {|char| hash[char] = text.count(char)}
+    print hash, "\n"
+    length = []
+    'balon'.chars.each {|char| 
+      if char =~ /[lo]/ and hash[char]
+        length << hash[char] / 2
+      elsif hash[char]
+        length << hash[char]
+      else
+        return 0
+      end
+    }
+    length.min
+  end
+end

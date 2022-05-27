4d1d2976351e291fb3cd378fa64178dae1f447ae
Mahima-01 <102168844+Mahima-01@users.noreply.github.com>
2022-05-27 21:57:43 +0530

    Practised confirmation in validation.

--- Rails/Library/app/models/person.rb
+++ Rails/Library/app/models/person.rb
@@ -1,25 +1,18 @@
 class Person < ApplicationRecord
-  validates :checkbox, acceptance: {accept: 1}
+  validates :name, presence: true
+  validates :name, confirmation: {case_sensitive: false}
+  validates :name_confirmation, presence: true
 end
+
 
 =begin
-# acceptance:
-This method validates that a checkbox on the user interface was checked when a form was submitted. 
-This is typically used when the user needs to agree to your application's terms of service, confirm that some text is read, or any similar concept.
-It usually takes value in form of true or 1. 
-class Person < ApplicationRecord
-  validates :checkbox, acceptance: true
-end
-3.0.0 :011 > person.save
-  TRANSACTION (0.2ms)  BEGIN
-  Person Create (0.6ms)  INSERT INTO "people" ("name", "gender", "dob", "created_at", "updated_at") VALUES ($1, $2, $3, $4, $5) RETURNING "id"  [["name", nil], ["gender", nil], ["dob", nil], ["created_at", "2022-05-27 13:51:37.899836"], ["updated_at", "2022-05-27 13:51:37.899836"]]
-  TRANSACTION (6.8ms)  COMMIT                   
- => true 
-
- 3.0.0 :002 > person.checkbox = 1
- => 1 
- 3.0.0 :006 > person.checkbox = true
- => true 
- 3.0.0 :007 > person.save
- => false 
+You should use this helper when you have two text fields that should receive exactly the same content. 
+For example, you may want to confirm an email address or a password. 
+This validation creates a virtual attribute whose name is the name of the field that has to be confirmed with "_confirmation" appended.
+3.0.0 :001 > person = Person.new
+ => #<Person:0x00005575ec4976b0 id: nil, name: nil, gender: nil, dob: nil, created_at: nil, updated_at: nil> 
+3.0.0 :002 > person.name_confirmation = 'Ram'
+ => "Ram"
+3.0.0 :005 > person.name_confirmation = 'ram'
+ => "ram" 
 =end

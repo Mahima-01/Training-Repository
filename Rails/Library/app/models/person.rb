a0481dc96bb0f9551cf46c16c5b4adadcbc0e315
Mahima-01 <102168844+Mahima-01@users.noreply.github.com>
2022-05-31 14:49:20 +0530

    Practised inclusion in validation.

--- Rails/Library/app/models/person.rb
+++ Rails/Library/app/models/person.rb
@@ -1,18 +1,21 @@
 class Person < ApplicationRecord
-  validates :name, presence: true
-  validates :name, confirmation: {case_sensitive: false}
-  validates :name_confirmation, presence: true
+  validates :name, inclusion: { in: %w[Mahima Rishabh] }
 end
 
 
 =begin
-You should use this helper when you have two text fields that should receive exactly the same content. 
-For example, you may want to confirm an email address or a password. 
-This validation creates a virtual attribute whose name is the name of the field that has to be confirmed with "_confirmation" appended.
+# inclusion:
+It checks the value can only be any of those on the in option.
 3.0.0 :001 > person = Person.new
- => #<Person:0x00005575ec4976b0 id: nil, name: nil, gender: nil, dob: nil, created_at: nil, updated_at: nil> 
-3.0.0 :002 > person.name_confirmation = 'Ram'
- => "Ram"
-3.0.0 :005 > person.name_confirmation = 'ram'
- => "ram" 
+ => #<Person:0x00007effa864cc20 id: nil, name: nil, gender: nil, dob: nil, created_at: nil, updated_at: nil> 
+3.0.0 :002 > person.valid?
+ => false 
+3.0.0 :003 > person.name = "Rishabh"
+ => "Rishabh" 
+3.0.0 :004 > person.valid?
+ => true 
+ 3.0.0 :005 > person.name = "Lalit"
+ => "Lalit" 
+3.0.0 :006 > person.valid?
+ => false 
 =end

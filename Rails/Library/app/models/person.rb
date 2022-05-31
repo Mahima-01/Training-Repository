64d6cf678bfd828b83ad14b91b73da50afedbb00
Mahima-01 <102168844+Mahima-01@users.noreply.github.com>
2022-05-31 15:01:57 +0530

    Practised length in validation.

--- Rails/Library/app/models/person.rb
+++ Rails/Library/app/models/person.rb
@@ -1,21 +1,30 @@
 class Person < ApplicationRecord
-  validates :name, inclusion: { in: %w[Mahima Rishabh] }
+  validates :name, length: { maximum: 4 }
+  validates :gender, length: { maximum: 5 }
 end
 
 
 =begin
-# inclusion:
-It checks the value can only be any of those on the in option.
-3.0.0 :001 > person = Person.new
- => #<Person:0x00007effa864cc20 id: nil, name: nil, gender: nil, dob: nil, created_at: nil, updated_at: nil> 
-3.0.0 :002 > person.valid?
- => false 
-3.0.0 :003 > person.name = "Rishabh"
- => "Rishabh" 
-3.0.0 :004 > person.valid?
- => true 
- 3.0.0 :005 > person.name = "Lalit"
- => "Lalit" 
+# length:
+This helper validates the length of the attributes' values. It provides a variety of options, so you can specify length constraints in different ways:
+3.0.0 :005 > person.name = 'Ella'
+ => "Ella" 
 3.0.0 :006 > person.valid?
+ => true
+3.0.0 :002 > person.name = 'Ram'
+ => "Ram" 
+3.0.0 :003 > person.valid?
+ => true 
+3.0.0 :002 > person.gender = 'Male'
+ => "Male" 
+3.0.0 :003 > person.valid?
+ => true 
+3.0.0 :004 > person.gender = 'Female'
+ => "Female" 
+3.0.0 :005 > person.valid?
+ => false 
+3.0.0 :004 > person.name = 'Manjeet'
+ => "Manjeet" 
+3.0.0 :005 > person.valid?
  => false 
 =end

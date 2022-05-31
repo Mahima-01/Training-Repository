c5b8a5ce50086effce41abe804da4e0f44a6210a
Mahima-01 <102168844+Mahima-01@users.noreply.github.com>
2022-05-31 18:46:29 +0530

    Practised allow_blank in validations.

--- Rails/Library/app/models/coffee.rb
+++ Rails/Library/app/models/coffee.rb
@@ -1,25 +1,31 @@
 class Coffee < ApplicationRecord
-  validates :size, inclusion: { in: %w(small medium large),
-  message: "%{value} is not a valid size" }, allow_nil: true
+  validates :brand, length: { is: 9 }, allow_blank: true
 end
 
-
 =begin
-# allow_nill:
-The :allow_nil option skips the validation when the value being validated is nil
-3.0.0 :005 > coffee.brand = 'nescafe'
- => "nescafe"
- 3.0.0 :006 > coffee.valid?
- => true 
- 3.0.0 :007 > coffee.size = 'small'
- => "small" 
-3.0.0 :008 > coffee.valid?
- => true 
- 3.0.0 :009 > coffee.size = 'extra large'
- => "extra large" 
-3.0.0 :010 > coffee.valid?
+# allow_blank:
+The :allow_blank option is similar to the :allow_nil option. 
+This option will let validation pass if the attribute's value is blank?, like nil or an empty string.
+3.0.0 :002 > coffee = Coffee.new
+ => #<Coffee:0x000055c1104b1118 id: nil, brand: nil, size: nil, created_at: nil, updated_at: nil> 
+3.0.0 :003 > coffee.brand = 'nescafe'
+ => "nescafe" 
+3.0.0 :004 > coffee.valid?
  => false 
- 3.0.0 :011 > coffee.size = nil
- => nil 
-3.0.0 :012 > coffee.valid?
+3.0.0 :005 > coffee.brand = 'bru'
+ => "bru" 
+3.0.0 :006 > coffee.valid?
+ => false
+3.0.0 :002 > coffee.brand = 'Caffeinee'
+ => "Caffeinee" 
+3.0.0 :003 > coffee.valid?
  => true 
+3.0.0 :004 > coffee.brand = 'Manhattan Coffee'
+ => "Manhattan Coffee" 
+3.0.0 :005 > coffee.valid?
+ => false 
+3.0.0 :006 > coffee.brand = nil
+ => nil 
+3.0.0 :007 > coffee.valid?
+ => true 
+=end

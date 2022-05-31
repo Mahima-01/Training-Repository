7af15498c999566501d96042801e17a2c6b21f00
Mahima-01 <102168844+Mahima-01@users.noreply.github.com>
2022-05-31 15:17:36 +0530

    Practised numericality in validation.

--- Rails/Library/app/models/person.rb
+++ Rails/Library/app/models/person.rb
@@ -1,30 +1,24 @@
 class Person < ApplicationRecord
-  validates :name, length: { maximum: 4 }
-  validates :gender, length: { maximum: 5 }
+  validates :id, numericality: true
+  validates :id, numericality: { only_integer: true }
 end
 
-
 =begin
-# length:
-This helper validates the length of the attributes' values. It provides a variety of options, so you can specify length constraints in different ways:
-3.0.0 :005 > person.name = 'Ella'
- => "Ella" 
-3.0.0 :006 > person.valid?
- => true
-3.0.0 :002 > person.name = 'Ram'
- => "Ram" 
-3.0.0 :003 > person.valid?
+# numericality:
+This helper validates that your attributes have only numeric values. 
+By default, it will match an optional sign followed by an integer or floating point number.
+To specify that only integer numbers are allowed, set :only_integer to true
+By default, numericality doesn't allow nil values. You can use allow_nil: true option to permit it.
+3.0.0 :007 > person.id = 20 
+ => 20 
+3.0.0 :008 > person.valid?
  => true 
-3.0.0 :002 > person.gender = 'Male'
- => "Male" 
-3.0.0 :003 > person.valid?
- => true 
-3.0.0 :004 > person.gender = 'Female'
- => "Female" 
-3.0.0 :005 > person.valid?
- => false 
-3.0.0 :004 > person.name = 'Manjeet'
- => "Manjeet" 
-3.0.0 :005 > person.valid?
+ 3.0.0 :009 > person.id = 20.1 
+ => 20.1 
+3.0.0 :010 > person.valid?
+ => false
+ 3.0.0 :012 > person.id = 21205679845342r
+ => (21205679845342/1) 
+3.0.0 :013 > person.valid?
  => false 
 =end

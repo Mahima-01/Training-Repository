cfb6ee5b514330c2f461fbc9cf7d327e2ef6325c
Mahima-01 <102168844+Mahima-01@users.noreply.github.com>
2022-06-01 15:29:22 +0530

    Practised Strict Validation.

--- Rails/Library/app/models/person.rb
+++ Rails/Library/app/models/person.rb
@@ -1,34 +1,17 @@
 class Person < ApplicationRecord
-  validates :name, presence: true, length: { minimum: 3 }
+  validates :name, presence: { strict: true }
 end
 
 =begin
-# errors.clear
-The clear method is used when you intentionally want to clear the errors collection. Of course, calling errors.clear upon an invalid object won't actually make it valid: the errors collection will now be empty, 
-but the next time you call valid? or any method that tries to save this object to the database, the validations will run again. If any of the validations fail, the errors collection will be filled again.
+# Strict validation:
+You can also specify validations to be strict and 
+raise ActiveModel::StrictValidationFailed when the object is invalid.
 3.0.0 :001 > person = Person.new
- => #<Person:0x000055c0363fa0d8 id: nil, name: nil, gender: nil, dob: nil, created_at: nil, updated_at: nil> 
-3.0.0 :002 > person.valid?
- => false 
-3.0.0 :003 > person.errors.messages
- => {:name=>["can't be blank", "is too short (minimum is 3 characters)"]} 
-3.0.0 :004 > person.name = 'Raman'
+ => #<Person:0x00007f4b18917610 id: nil, name: nil, gender: nil, dob: nil, created_at: nil, updated_at: nil> 
+ 3.0.0 :002 > person.valid?
+ /home/itsacheckmate/.rvm/gems/ruby-3.0.0/gems/activemodel-7.0.3/lib/active_model/errors.rb:323:in `add': Name can't be blank (ActiveModel::StrictValidationFailed)
+3.0.0 :003 > person.name = 'Raman'
  => "Raman" 
-3.0.0 :005 > person.valid?
- => true
-3.0.0 :007 > person.errors.clear
- => [] 
-3.0.0 :008 > person.errors.empty?
+3.0.0 :004 > person.valid?
  => true 
-# errors.size:
-The size method returns the total number of errors for the object
-3.0.0 :009 > person.errors.size
- => 0 
-3.0.0 :001 > person = Person.new
- => 
-#<Person:0x0000564ba5397730
-3.0.0 :002 > person.valid?
- => false 
-3.0.0 :003 > person.errors.size
- => 2 
 =end

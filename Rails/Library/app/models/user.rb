a3d0572b631419a5fda13e19bba569ceeb75bcaf
Mahima-01 <102168844+Mahima-01@users.noreply.github.com>
2022-06-01 21:25:28 +0530

    Practised conditional validations

--- Rails/Library/app/models/user.rb
+++ Rails/Library/app/models/user.rb
@@ -1,39 +1,37 @@
 =begin
 class User < ApplicationRecord
-  validates :last_name, presence: true, if: :admin? 
-  def admin?
-    !first_name.blank?
-  end
+  validates :first_name, presence: true, if: Proc.new { |user| user.last_name.blank? }
+end
+class User < ApplicationRecord
+  validates :first_name, presence: true, unless: Proc.new { |user| user.last_name.present? }
 end
 =end
-
 class User < ApplicationRecord
-  validates :first_name, presence: true, if: :admin? 
-  def admin?
-    !last_name.blank?
+   with_options if: :is_admin? do |admin|
+    admin.validates :first_name, length: { minimum: 10 }
+    admin.validates :last_name, presence: true
   end
 end
 
+
+
 =begin
-5.1 Using a Symbol with :if and :unless
-You can associate the :if and :unless options with a symbol corresponding to the name of a method that will get called right before validation happens. 
-This is the most commonly used option.
-3.0.0 :001 > user = User.new
- => #<User:0x0000555dce0f2860 id: nil, first_name: nil, last_name: nil, created_at: nil, updated_at: nil> 
-3.0.0 :002 > user.first_name = 'Raman'
- => "Raman" 
-3.0.0 :003 > user.valid?
- => false 
-3.0.0 :004 > user = User.new
- => #<User:0x00007f0018a589c0 id: nil, first_name: nil, last_name: nil, created_at: nil, updated_at: nil> 
-3.0.0 :005 > user.valid?
+It is possible to associate :if and :unless with a Proc object which will be called. 
+Using a Proc object gives you the ability to write an inline condition instead of a separate method. This option is best suited for one-liners.
+3.0.0 :003 > user.first_name = 'Rishabh'
+ => "Rishabh" 
+3.0.0 :004 > user.valid?
  => true 
- 3.0.0 :002 > user.last_name = 'Sharma'
- => "Sharma" 
-3.0.0 :003 > user.valid?
- => false 
-3.0.0 :004 > user = User.new
- => #<User:0x000055e6d2f3c9d8 id: nil, first_name: nil, last_name: nil, created_at: nil, updated_at: nil> 
-3.0.0 :005 > user.valid?
+ 3.0.0 :005 > user.first_name = nil
+ => nil 
+3.0.0 :006 > user.valid?
  => true 
+3.0.0 :007 > user.last_name = 'gaur'
+ => "gaur" 
+3.0.0 :008 > user.valid?
+ => true 
+3.0.0 :009 > user = User.new
+ => #<User:0x00007f85d0a71ec8 id: nil, first_name: nil, last_name: nil, created_at: nil, updated_at: nil> 
+3.0.0 :010 > user.valid?
+ => false 
 =end

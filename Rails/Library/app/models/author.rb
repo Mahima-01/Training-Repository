7c89f5af93301766cb3d8d04aeb24b40a699ce76
Mahima-01 <102168844+Mahima-01@users.noreply.github.com>
2022-06-02 23:03:54 +0530

    Practised belongs_to and has_many in association.

--- Rails/Library/app/models/author.rb
+++ Rails/Library/app/models/author.rb
@@ -1,5 +1,16 @@
 class Author < ApplicationRecord
-  validates :email, uniqueness: true, on: :create
-  validates :title, presence: true
+  has_many :books, dependent: :destroy
 end
- 
+
+class Book < ApplicationRecord
+  belongs_to :author
+end
+
+=begin
+A belongs_to association sets up a connection with another model, 
+such that each instance of the declaring model "belongs to" one instance of the other model.
+
+A has_many association is similar to has_one, but indicates a one-to-many connection with another model. 
+You'll often find this association on the "other side" of a belongs_to association. 
+This association indicates that each instance of the model has zero or more instances of another model.
+=end
--- Rails/Library/app/models/book.rb
+++ Rails/Library/app/models/book.rb
@@ -1,2 +1,8 @@
 class Book < ApplicationRecord
+  belongs_to :author
 end
+
+=begin
+A belongs_to association sets up a connection with another model, 
+such that each instance of the declaring model "belongs to" one instance of the other model.
+=end

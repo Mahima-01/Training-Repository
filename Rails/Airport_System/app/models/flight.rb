ae557871e104555b26946436c27807ae54cdd7a3
Mahima-01 <102168844+Mahima-01@users.noreply.github.com>
2022-06-03 20:52:24 +0530

    Practised has_one in associations.

--- Rails/Airport_System/app/models/flight.rb
+++ Rails/Airport_System/app/models/flight.rb
@@ -1,2 +1,7 @@
 class Flight < ApplicationRecord
+  has_one :aircraft
 end
+
+# A has_one association indicates that one other model has a reference to this model. That model can be fetched through this association.
+# A has_one :through association sets up a one-to-one connection with another model.
+

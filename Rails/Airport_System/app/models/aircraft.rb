7f232abd7781852ea265997fabed9ac76853ac22
Mahima-01 <102168844+Mahima-01@users.noreply.github.com>
2022-06-03 21:03:06 +0530

    Practised has_many through associations.

--- Rails/Airport_System/app/models/aircraft.rb
+++ Rails/Airport_System/app/models/aircraft.rb
@@ -1,2 +1,9 @@
 class Aircraft < ApplicationRecord
+  has_many :flights_aircrafts
+  has_many :flights, through: :flights_aircrafts
 end
+
+# Has_many: through association sets up a many-to-many association using another model. 
+#It creates direct many-to-many connections with another model
+# In this association, we need three models to make it work.
+
--- Rails/Airport_System/app/models/flight.rb
+++ Rails/Airport_System/app/models/flight.rb
@@ -1,12 +1,8 @@
 class Flight < ApplicationRecord
-  has_one :aircraft
   has_many :aircrafts
+  has_many :flights_aircrafts
+  has_many :aircrafts, through: :flights_aircrafts
 end
 
-=begin
-A has_many :through association 
-Is often used to set up a many-to-many connection with another model. 
-This association indicates that the declaring model can be matched with zero or more instances of another model by proceeding through a third model. 
-=end
 
 

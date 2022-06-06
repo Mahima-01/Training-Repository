9ee02e9f6ce1616cd8e51d4a3c352ea19aad2de9
Mahima-01 <102168844+Mahima-01@users.noreply.github.com>
2022-06-06 17:27:00 +0530

    Practised build_association(attributes = {})

--- Rails/Library/app/models/book.rb
+++ Rails/Library/app/models/book.rb
@@ -3,33 +3,31 @@ class Book < ApplicationRecord
 end
 
 =begin
-A belongs_to association sets up a connection with another model, 
-such that each instance of the declaring model "belongs to" one instance of the other model.
-association
-The association method returns the associated object, if any. If no associated object is found, it returns nil.
-3.0.0 :001 > book = Book.new
- => #<Book:0x00007fe4d0971708 id: nil, title: nil, author: nil, created_at: nil, updated_at: nil, price: nil, lock_version: 0> 
-3.0.0 :009 > author = Author.new
- => #<Author:0x00007f83d8d13748 id: nil, created_at: nil, updated_at: nil, lock_version: 0, email: nil, name: nil> 
-3.0.0 :010 > author.save
+build_association(attributes = {})
+The build_association method returns a new object of the associated type. 
+This object will be instantiated from the passed attributes, and the link through this object's foreign key will be set, but the associated object will not yet be saved.
+3.0.0 :019 > book1 = Book.new
+ => #<Book:0x000056169a231f70 id: nil, title: nil, author: nil, created_at: nil, updated_at: nil, price: nil, lock_version: 0, author_id: nil> 
+3.0.0 :026 > book.save
   TRANSACTION (0.2ms)  BEGIN
-  Author Create (0.2ms)  INSERT INTO "authors" ("created_at", "updated_at", "lock_version", "email", "name") VALUES ($1, $2, $3, $4, $5) RETURNING "id"  [["created_at", "2022-06-06 11:24:14.191542"], ["updated_at", "2022-06-06 11:24:14.191542"], ["lock_version", 0], ["email", nil], ["name", nil]]
-  TRANSACTION (5.5ms)  COMMIT                                                  
- => true   
-3.0.0 :011 > book = author.books.new
- => #<Book:0x000056169a228150 id: nil, title: nil, author: nil, created_at: nil, updated_at: nil, price: nil, lock_version: 0, author_id: 1000024> 
-3.0.0 :013 > book.save
+  Book Update (0.4ms)  UPDATE "books" SET "title" = $1, "updated_at" = $2, "lock_version" = $3 WHERE "books"."id" = $4 AND "books"."lock_version" = $5  [["title", "Mathematics"], ["updated_at", "2022-06-06 11:36:45.319017"], ["lock_version", 1], ["id", 5], ["lock_version", 0]]
+  TRANSACTION (6.6ms)  COMMIT                    
  => true 
-3.0.0 :015 > book.title = 'Mathematics'
- => "Mathematics" 
-3.0.0 :016 > book.author
+3.0.0 :029 > book = book1.build_author(name: 'Joe', email: 'joe12@gmail.com')
+ => #<Author:0x00007f83d8dcfd80 id: nil, created_at: nil, updated_at: nil, lock_version: 0, email: "joe12@gmail.com", name: "Joe"> 
+3.0.0 :030 > book.save
+  TRANSACTION (0.3ms)  BEGIN
+  Author Create (0.6ms)  INSERT INTO "authors" ("created_at", "updated_at", "lock_version", "email", "name") VALUES ($1, $2, $3, $4, $5) RETURNING "id"  [["created_at", "2022-06-06 11:45:17.961855"], ["updated_at", "2022-06-06 11:45:17.961855"], ["lock_version", 0], ["email", "joe12@gmail.com"], ["name", "Joe"]]
+  TRANSACTION (1.5ms)  COMMIT                    
+ => true 
+3.0.0 :031 > book1.author
  => 
-#<Author:0x00007f83d8d13748                                                    
- id: 1000024,                                                                  
- created_at: Mon, 06 Jun 2022 11:24:14.191542000 UTC +00:00,                   
- updated_at: Mon, 06 Jun 2022 11:24:14.191542000 UTC +00:00,                   
- lock_version: 0,                                                              
- email: nil,                                                                   
- name: nil> 
+#<Author:0x00007f83d8dcfd80                                              
+ id: 1000025,                                                            
+ created_at: Mon, 06 Jun 2022 11:45:17.961855000 UTC +00:00,             
+ updated_at: Mon, 06 Jun 2022 11:45:17.961855000 UTC +00:00,             
+ lock_version: 0,                                                        
+ email: "joe12@gmail.com",                                               
+ name: "Joe"> 
 =end
 

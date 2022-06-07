403640d5b83d6221b50a30d06c646197f632f8ca
Mahima-01 <102168844+Mahima-01@users.noreply.github.com>
2022-06-07 13:38:35 +0530

    Bi-directional Associations

--- Rails/Library/app/models/book.rb
+++ Rails/Library/app/models/book.rb
@@ -2,4 +2,9 @@ class Book < ApplicationRecord
   belongs_to :author
 end
 
-
+=begin
+A bidirectional relationship is valid in both directions. 
+Intersects is an example of a bidirectional relationship.
+Active Record will attempt to automatically identify that these two models share a bi-directional association based on the association name. 
+In this way, Active Record will only load one copy of the Author object, making your application more efficient and preventing inconsistent data
+=end

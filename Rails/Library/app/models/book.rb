6a426d0aac20ea37ac56a541ea83da25241448d8
Mahima-01 <102168844+Mahima-01@users.noreply.github.com>
2022-06-07 17:12:57 +0530

    :foreign_key in belong_to option.

--- Rails/Library/app/models/book.rb
+++ Rails/Library/app/models/book.rb
@@ -1,14 +1,11 @@
 class Book < ApplicationRecord
-  belongs_to :author, counter_cache: true
+  belongs_to :author, class_name: "Author", foreign_key: "author_id"
 end
 
 
 =begin
-# :counter_cache
-The :counter_cache option can be used to make finding the number of belonging objects more efficient.
-Although the :counter_cache option is specified on the model that includes the belongs_to declaration, 
-the actual column must be added to the associated (has_many) model. 
-3.0.0 :012 > author.books.size
-  Book Count (1.9ms)  SELECT COUNT(*) FROM "books" WHERE "books"."author_id" = $1  [["author_id", 1000027]]
- => 0  
+# :foreign_key
+By convention, Rails assumes that the column used to hold the foreign key on this model is the name of the association with the suffix _id added. 
+The :foreign_key option lets you set the name of the foreign key directly:
+In any case, Rails will not create foreign key columns for you. You need to explicitly define them as part of your migrations.
 =end

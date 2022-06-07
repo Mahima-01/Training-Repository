1c047da68356c67512beb0ba357eba80d65fb277
Mahima-01 <102168844+Mahima-01@users.noreply.github.com>
2022-06-07 18:08:46 +0530

    Add files via upload

--- Rails/Library/app/models/author.rb
+++ Rails/Library/app/models/author.rb
@@ -1,29 +1,5 @@
 class Author < ApplicationRecord
-  has_many :books, dependent: :destroy
+  self.primary_key = 'guid'
+  has_many :books, inverse_of: :author
 end
 
-
- 
-=begin                                
-3.0.0 :008 > book2 = Book.new
- => #<Book:0x000055f9eb7e2260 id: nil, title: nil, author: nil, created_at: nil, updated_at: nil, price: nil, lock_version: 0, author_id: nil> 
-3.0.0 :009 > book = book2.create_author(name: 'Joe', email: 'joe12@gmail.com')
-  TRANSACTION (0.1ms)  BEGIN
-  Author Create (0.2ms)  INSERT INTO "authors" ("created_at", "updated_at", "lock_version", "email", "name") VALUES ($1, $2, $3, $4, $5) RETURNING "id"  [["created_at", "2022-06-07 07:49:57.701935"], ["updated_at", "2022-06-07 07:49:57.701935"], ["lock_version", 0], ["email", "joe12@gmail.com"], ["name", "Joe"]]
-  TRANSACTION (6.5ms)  COMMIT                             
- =>                                                       
-#<Author:0x00007fb0a4173480
-3.0.0 :010 > book2.author
- => 
-#<Author:0x00007fb0a4173480                                                        
- id: 1000030,                                                                      
- created_at: Tue, 07 Jun 2022 07:49:57.701935000 UTC +00:00,                       
- updated_at: Tue, 07 Jun 2022 07:49:57.701935000 UTC +00:00,                       
- lock_version: 0,                                                                  
- email: "joe12@gmail.com",                                                         
- name: "Joe"> 
-3.0.0 :016 > author = book2.author
- => 
-#<Author:0x00007fb0a4173480  
-
-=end
--- Rails/Library/app/models/book.rb
+++ Rails/Library/app/models/book.rb
@@ -1,11 +1,16 @@
 class Book < ApplicationRecord
-  belongs_to :author, class_name: "Author", foreign_key: "author_id"
+  belongs_to :author, primary_key: 'guid'
+  belongs_to :author, inverse_of: :books
 end
 
 
 =begin
-# :foreign_key
-By convention, Rails assumes that the column used to hold the foreign key on this model is the name of the association with the suffix _id added. 
-The :foreign_key option lets you set the name of the foreign key directly:
-In any case, Rails will not create foreign key columns for you. You need to explicitly define them as part of your migrations.
+:primary_key
+By convention, Rails assumes that the id column is used to hold the primary key of its tables. 
+By convention, all database tables in rails have a primary-key of the id column.
+If, for some odd reason (eg you've got a legacy database), your table uses a different primary key to id... you use the primary_key call to tell rails what it is.
+The :primary_key option allows you to specify a different column.
+
+:inverse_of
+The :inverse_of option specifies the name of the has_many or has_one association that is the inverse of this association.
 =end

96483d53008b8a00c324b7b6194f83716cdbd59f
Mahima-01 <102168844+Mahima-01@users.noreply.github.com>
2022-06-07 14:52:05 +0530

    Practised :class_name in belongs_to option.

--- Rails/Library/app/models/book.rb
+++ Rails/Library/app/models/book.rb
@@ -1,10 +1,5 @@
 class Book < ApplicationRecord
-  belongs_to :author
+  belongs_to :author, class_name: "Author"
 end
 
-=begin
-A bidirectional relationship is valid in both directions. 
-Intersects is an example of a bidirectional relationship.
-Active Record will attempt to automatically identify that these two models share a bi-directional association based on the association name. 
-In this way, Active Record will only load one copy of the Author object, making your application more efficient and preventing inconsistent data
-=end
+

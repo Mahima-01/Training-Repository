9ad6cac071b5dcbd6392cacb83cdb11962bc8dc3
Mahima-01 <102168844+Mahima-01@users.noreply.github.com>
2022-06-09 15:43:52 +0530

    Created routes for controllers for hms.

--- Rails/hms/config/routes.rb
+++ Rails/hms/config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+   root "homepage#index"
+   get '/p', to: 'homepage#patients'
 end

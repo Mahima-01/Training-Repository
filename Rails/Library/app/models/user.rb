f708977cccbdbd5efa82721a22b337031f7add8f
Mahima-01 <102168844+Mahima-01@users.noreply.github.com>
2022-06-13 18:13:46 +0530

    Dynamic Finders in query interface.

--- Rails/Library/app/models/user.rb
+++ Rails/Library/app/models/user.rb
@@ -6,52 +6,50 @@ class User < ApplicationRecord
 end
 
 =begin
-# Finding by Sql:
-1.select_all
-2.Pluck
-3.ids
+# Dynamic Finders:
+For every field (also known as an attribute) you define in your table, Active Record provides a finder method. 
+If you have a field called first_name on your Customer model for example, you get the instance method find_by_first_name for free from Active Record. If you also have a locked field on the Customer model, you also get find_by_locked method.
 
-# select_all:
-3.0.0 :086 >  User.connection.select_all("SELECT first_name, created_at FROM users WHERE id = '1'").to_a
-   (0.3ms)  SELECT first_name, created_at FROM users WHERE id = '1'
- => [{"first_name"=>"Mary", "created_at"=>2022-06-09 13:33:43.42781 UTC}]   
- 
-# Pluck:
-pluck
-pluck can be used to query single or multiple columns from the underlying table of a model. 
-It accepts a list of column names as an argument and returns an array of values of the specified columns with the corresponding data type.
-3.0.0 :083 >  User.where(first_name: 'Nina').pluck(:id)
-  User Pluck (0.6ms)  SELECT "users"."id" FROM "users" WHERE "users"."first_name" = $1  [["first_name", "Nina"]]
- => [9] 
-Unlike select, pluck directly converts a database result into a Ruby Array, without constructing ActiveRecord objects. 
-This can mean better performance for a large or frequently-run query.
-3.0.0 :087 >  User.pluck(:id)
-  User Pluck (0.6ms)  SELECT "users"."id" FROM "users"
- => [1, 4, 2, 5, 3, 6, 7, 8, 9, 10, 11] 
-3.0.0 :088 >  User.pluck(:id, :first_name)
-  User Pluck (0.5ms)  SELECT "users"."id", "users"."first_name" FROM "users"
- => [[1, "Mary"], [4, "Manu"], [2, "Mridul"], [5, "Mahima"], [3, "Mridula"], [6, "Andy"], [7, "Raman"], [8, "Aryan"], [9, "Nina"], [10, nil], [11, "Larry"]] 
- 
-limit does not work with pluck, it raises an error.
-3.0.0 :091 > User.pluck(:first_name).limit(4)
-  User Pluck (0.5ms)  SELECT "users"."first_name" FROM "users"
-(irb):91:in `<main>': undefined method `limit' for ["Mary", "Manu", "Mridul", "Mahima", "Mridula", "Andy", "Raman", "Aryan", "Nina", nil, "Larry"]:Array (NoMethodError) 
+You can specify an exclamation point (!) on the end of the dynamic finders to get them to raise an ActiveRecord::RecordNotFound error if they do not return any records, like Customer.find_by_name!("Ryan")
+3.0.0 :100 > User.find_by_name!("Mridula")
+/home/itsacheckmate/.rvm/gems/ruby-3.0.0/gems/activerecord-7.0.3/lib/active_record/dynamic_matchers.rb:22:in `method_missing': undefined method `find_by_name!' for User:Class (NoMethodError)                       
+Did you mean?  find_by! 
 
-3.0.0 :092 > User.limit(2).pluck(:first_name)
-  User Pluck (0.1ms)  SELECT "users"."first_name" FROM "users" LIMIT $1  [["LIMIT", 2]]
- => ["Mary", "Manu"] 
+3.0.0 :103 > User.find_by_first_name_and_teacher_id("Nina", 5)
+  User Load (0.2ms)  SELECT "users".* FROM "users" WHERE "users"."first_name" = $1 AND "users"."teacher_id" = $2 LIMIT $3  [["first_name", "Nina"], ["teacher_id", 5], ["LIMIT", 1]]                                                                                                   
+ => nil 
+3.0.0 :108 > User.find_by_first_name_and_teacher_id("Mahima",7 )
+  User Load (0.5ms)  SELECT "users".* FROM "users" WHERE "users"."first_name" = $1 AND "users"."teacher_id" = $2 LIMIT $3  [["first_name", "Mahima"], ["teacher_id", 7], ["LIMIT", 1]]                      
+ =>                                                
+#<User:0x00007f7f8872bf00
+ id: 5,
+ first_name: "Mahima",
+ last_name: nil,
+ created_at: Thu, 09 Jun 2022 14:20:22.722410000 UTC +00:00,
+ updated_at: Thu, 09 Jun 2022 14:21:12.488182000 UTC +00:00,
+ type: nil,
+ teacher_id: 7> 
+3.0.0 :109 > User.find_by_first_name("Mahima")
+  User Load (0.5ms)  SELECT "users".* FROM "users" WHERE "users"."first_name" = $1 LIMIT $2  [["first_name", "Mahima"], ["LIMIT", 1]]
+ => 
+#<User:0x00007f7f8869d728
+ id: 5,
+ first_name: "Mahima",
+ last_name: nil,
+ created_at: Thu, 09 Jun 2022 14:20:22.722410000 UTC +00:00,
+ updated_at: Thu, 09 Jun 2022 14:21:12.488182000 UTC +00:00,
+ type: nil,
+ teacher_id: 7> 
 
-3.0.0 :097 > assoc.unscope(:includes).pluck(:id)
-  User Pluck (0.4ms)  SELECT "users"."id" FROM "users"
- => [1, 4, 2, 5, 3, 6, 7, 8, 9, 10, 11]
-
-# ids
-ids can be used to pluck all the IDs for the relation using the table's primary key.
-3.0.0 :098 > User.ids
-  User Pluck (0.7ms)  SELECT "users"."id" FROM "users"
- => [1, 4, 2, 5, 3, 6, 7, 8, 9, 10, 11] 
-
-                                            
- 
- 
+3.0.0 :110 > User.find_by_first_name_and_teacher_id("Mridula",4 )
+  User Load (0.3ms)  SELECT "users".* FROM "users" WHERE "users"."first_name" = $1 AND "users"."teacher_id" = $2 LIMIT $3  [["first_name", "Mridula"], ["teacher_id", 4], ["LIMIT", 1]]
+ => 
+#<Admin:0x000055a9fb1fbb20
+ id: 3,
+ first_name: "Mridula",
+ last_name: nil,
+ created_at: Thu, 09 Jun 2022 13:38:04.971554000 UTC +00:00,
+ updated_at: Thu, 09 Jun 2022 14:32:46.658374000 UTC +00:00,
+ type: "Admin",
+ teacher_id: 4> 
 =end

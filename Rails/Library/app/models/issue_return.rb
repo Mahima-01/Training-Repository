d07ca0ea81eff7c7f72654d0b7c571b88f993987
Mahima-01 <102168844+Mahima-01@users.noreply.github.com>
2022-06-14 20:09:18 +0530

    Practised Enums in query interface.

--- Rails/Library/app/models/issue_return.rb
+++ Rails/Library/app/models/issue_return.rb
@@ -1,5 +1,6 @@
 class IssueReturn < ApplicationRecord
   validate :issue_date_cannot_be_in_the_past
+  enum :status, [:price, :status]
   def issue_date_cannot_be_in_the_past
     if issue_date.present? && issue_date < Date.today
       errors.add(:issue_date, "can't be in the past")
@@ -8,26 +9,18 @@ class IssueReturn < ApplicationRecord
 end
 
 =begin
-3.0.0 :001 > issue = IssueReturn.new
- => #<IssueReturn:0x00007fedc46cf638 id: nil, price: nil, issue_date: nil, return_date: nil, created_at: nil, updated_at: nil> 
- 3.0.0 :002 > issue.issue_date = '10-10-2021'
- => "10-10-2021" 
-3.0.0 :003 > issue.valid?
- => false 
-3.0.0 :004 > issue.errors.messages
- => {:issue_date=>["can't be in the past"]} 
-3.0.0 :007 > issue.issue_date = '02-06-2022'
- => "02-06-2022" 
-3.0.0 :008 > issue.valid?
- => true 
-3.0.0 :009 > issue.issue_date = '04-06-2022'
- => "04-06-2022" 
-3.0.0 :010 > issue.valid?
- => true 
-3.0.0 :011 > issue.issue_date = '12-12-2021'
- => "12-12-2021" 
-3.0.0 :012 > issue.valid?
- => false 
-3.0.0 :013 > issue.errors.messages
- => {:issue_date=>["can't be in the past"]} 
+# Enum:
+Enums
+An enum lets you define an Array of values for an attribute and refer to them by name. 
+The actual value stored in the database is an integer that has been mapped to one of the values.
+3.0.0 :001 > IssueReturn.price
+  IssueReturn Load (8.3ms)  SELECT "issue_returns".* FROM "issue_returns" WHERE "issue_returns"."status" = $1  [["status", 0]]
+ => []
+3.0.0 :002 > IssueReturn.status
+  IssueReturn Load (0.7ms)  SELECT "issue_returns".* FROM "issue_returns" WHERE "issue_returns"."status" = $1  [["status", 1]]
+ => [] 
+3.0.0 :003 > IssueReturn.not_status
+  IssueReturn Load (0.3ms)  SELECT "issue_returns".* FROM "issue_returns" WHERE "issue_returns"."status" != $1  [["status", 1]]
+ => [] 
+
 =end

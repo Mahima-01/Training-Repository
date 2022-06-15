fbdbd91345bff9cdcf1fa0ffad860c1a35b6bf8c
Mahima-01 <102168844+Mahima-01@users.noreply.github.com>
2022-06-15 18:42:53 +0530

    Practised after_commit in callbacks

--- Rails/Library/app/models/user.rb
+++ Rails/Library/app/models/user.rb
@@ -1,50 +1,39 @@
 class User < ApplicationRecord
   #has_many :students, class_name: "User", foreign_key: "teacher_id"
   #belongs_to :teacher, class_name: "User", optional:true
-  validates :login, :email, presence: true
-  around_create :ensure_login_has_a_value
-
-  def ensure_login_has_a_value
-    print "Callback used"
-    if type.nil?
-      self.type = email unless email.blank?
-    end
-  end
+  #validates :login, :email, presence: true
+  #around_create :ensure_login_has_a_value
+  after_commit :send_email_notifications, on: :create
+  
+  def send_email_notifications
+    # This will send email notifications in the background 
+    print "email notification sent."
+  end 
 end
 
 
 =begin
-# before_create:
-Registers a callback to be called before a record is created.
-3.0.0 :003 > user = User.create(login: "Dia")
- => 
-#<User:0x000055d6871bfd60
-3.0.0 :004 > user.save
- => false 
+# after_commit: 
+This callback is called after a record has been created, updated, or destroyed.
+You can specify that the callback should only be fired by a certain action with the :on option:
 
-3.0.0 :006 > user = User.create(email: "dia@gmail.com")
- => 
-#<User:0x000055d686d858f8  
-3.0.0 :010 > user.valid?
- => false 
+after_commit callback will ensure that the send_email_notifications 
+method is executed only when the commit transaction completes in the database.
 
-# after_create: 
-3.0.0 :005 > user = User.create(email: "dia@gmail.com", login: "Dia")
-  TRANSACTION (0.3ms)  BEGIN
-  User Create (0.5ms)  INSERT INTO "users" ("first_name", "last_name", "created_at", "updated_at", "type", "teacher_id", "login", "email") VALUES ($1, $2, $3, $4, $5, $6, $7, $8) RETURNING "id"  [["first_name", nil], ["last_name", nil], ["created_at", "2022-06-15 11:31:14.174630"], ["updated_at", "2022-06-15 11:31:14.174630"], ["type", nil], ["teacher_id", nil], ["login", "Dia"], ["email", "dia@gmail.com"]]
-Callback used  TRANSACTION (1.4ms)  COMMIT                                   
- =>                                                                          
-#<User:0x0000558ee344c678   
-
-# around_create:
-Registers a callback to be called around the creation of a record.
 3.0.0 :001 > user = User.new
- => #<User:0x00007fc520484878 id: nil, first_name: nil, last_name: nil, created_at: nil, updated_at: nil, type: nil, teacher_id: nil, login: nil, e... 
-3.0.0 :002 > user = User.create(email: "ria@gmail.com", login: "Ria")
-Callback used => 
-#<User:0x00007fc520387718   
-...                         
-3.0.0 :003 > user.valid?
- => true 
+ => #<User:0x00007ff9e0a67d90 id: nil, first_name: nil, last_name: nil, created_at: nil, updated_at: nil, type: nil, teacher_id: nil, login: nil, e... 
+3.0.0 :002 > user = User.create(email: "ria@gmail.com")
+  TRANSACTION (0.2ms)  BEGIN
+  User Create (0.9ms)  INSERT INTO "users" ("first_name", "last_name", "created_at", "updated_at", "type", "teacher_id", "login", "email") VALUES ($1, $2, $3, $4, $5, $6, $7, $8) RETURNING "id"  [["first_name", nil], ["last_name", nil], ["created_at", "2022-06-15 13:11:20.313029"], ["updated_at", "2022-06-15 13:11:20.313029"], ["type", nil], ["teacher_id", nil], ["login", nil], ["email", "ria@gmail.com"]]
+  TRANSACTION (6.5ms)  COMMIT
+email notification sent. => 
+#<User:0x00007ff9e0387160
 
+3.0.0 :003 > user = User.create(email: "manni@gmail.com")
+  TRANSACTION (0.2ms)  BEGIN
+  User Create (0.3ms)  INSERT INTO "users" ("first_name", "last_name", "created_at", "updated_at", "type", "teacher_id", "login", "email") VALUES ($1, $2, $3, $4, $5, $6, $7, $8) RETURNING "id"  [["first_name", nil], ["last_name", nil], ["created_at", "2022-06-15 13:12:00.397444"], ["updated_at", "2022-06-15 13:12:00.397444"], ["type", nil], ["teacher_id", nil], ["login", nil], ["email", "manni@gmail.com"]]
+  TRANSACTION (2.3ms)  COMMIT
+email notification sent. => 
+#<User:0x0000561fffa7e890
+...
 =end

50afbcbdd439c1c6ffbc5cfdb3adf6775f895f4e
Mahima-01 <102168844+Mahima-01@users.noreply.github.com>
2022-06-15 17:05:47 +0530

    Practised before_create, after_create and around_create

--- Rails/Library/app/models/user.rb
+++ Rails/Library/app/models/user.rb
@@ -1,60 +1,50 @@
 class User < ApplicationRecord
   #has_many :students, class_name: "User", foreign_key: "teacher_id"
   #belongs_to :teacher, class_name: "User", optional:true
-  validates :login, presence: true
-  before_save :call_before_save
-  after_save :call_after_save
-  around_save :call_around_save
-  def call_before_save
-    puts 'before user is saved'
-  end
+  validates :login, :email, presence: true
+  around_create :ensure_login_has_a_value
 
-  def call_after_save
-    puts 'after user is saved'
-  end
-
-  def call_around_save
-    puts 'in around user save'
-    yield # Article saved
-    puts 'out around user save'
+  def ensure_login_has_a_value
+    print "Callback used"
+    if type.nil?
+      self.type = email unless email.blank?
+    end
   end
 end
 
 
 =begin
-# around_save:
-Called before a new or existing object is saved until yield is invoked within the method triggered by the callback. 
-Calling yield causes the object to be saved and then any proceeding code in the method will execute.
-
-3.0.0 :004 > user = User.new(login: 'Kriti')
+# before_create:
+Registers a callback to be called before a record is created.
+3.0.0 :003 > user = User.create(login: "Dia")
  => 
-#<User:0x00007f192024d750 
-3.0.0 :005 > user.save
-before user is saved
-in around user save                           
-  TRANSACTION (0.1ms)  BEGIN                  
-  User Create (0.2ms)  INSERT INTO "users" ("first_name", "last_name", "created_at", "updated_at", "type", "teacher_id", "login", "email") VALUES ($1, $2, $3, $4, $5, $6, $7, $8) RETURNING "id"  [["first_name", nil], ["last_name", nil], ["created_at", "2022-06-15 10:07:50.712473"], ["updated_at", "2022-06-15 10:07:50.712473"], ["type", nil], ["teacher_id", nil], ["login", "Kriti"], ["email", nil]]
-out around user save                          
-after user is saved                           
-  TRANSACTION (1.3ms)  COMMIT                 
- => true       
-
-3.0.0 :006 > user = User.new(email: 'kriti@gmail.com')
- => 
-#<User:0x0000558705ae3990
-3.0.0 :007 > user.save
+#<User:0x000055d6871bfd60
+3.0.0 :004 > user.save
  => false 
 
-3.0.0 :008 > user = User.new(login: 'Sunny')
+3.0.0 :006 > user = User.create(email: "dia@gmail.com")
  => 
-#<User:0x00005587049ce858 
-3.0.0 :009 > user.save
-before user is saved
-in around user save                               
-  TRANSACTION (0.1ms)  BEGIN                  
-  User Create (0.2ms)  INSERT INTO "users" ("first_name", "last_name", "created_at", "updated_at", "type", "teacher_id", "login", "email") VALUES ($1, $2, $3, $4, $5, $6, $7, $8) RETURNING "id"  [["first_name", nil], ["last_name", nil], ["created_at", "2022-06-15 10:09:18.051881"], ["updated_at", "2022-06-15 10:09:18.051881"], ["type", nil], ["teacher_id", nil], ["login", "Sunny"], ["email", nil]]
-out around user save                          
-after user is saved                           
-  TRANSACTION (6.5ms)  COMMIT                 
+#<User:0x000055d686d858f8  
+3.0.0 :010 > user.valid?
+ => false 
+
+# after_create: 
+3.0.0 :005 > user = User.create(email: "dia@gmail.com", login: "Dia")
+  TRANSACTION (0.3ms)  BEGIN
+  User Create (0.5ms)  INSERT INTO "users" ("first_name", "last_name", "created_at", "updated_at", "type", "teacher_id", "login", "email") VALUES ($1, $2, $3, $4, $5, $6, $7, $8) RETURNING "id"  [["first_name", nil], ["last_name", nil], ["created_at", "2022-06-15 11:31:14.174630"], ["updated_at", "2022-06-15 11:31:14.174630"], ["type", nil], ["teacher_id", nil], ["login", "Dia"], ["email", "dia@gmail.com"]]
+Callback used  TRANSACTION (1.4ms)  COMMIT                                   
+ =>                                                                          
+#<User:0x0000558ee344c678   
+
+# around_create:
+Registers a callback to be called around the creation of a record.
+3.0.0 :001 > user = User.new
+ => #<User:0x00007fc520484878 id: nil, first_name: nil, last_name: nil, created_at: nil, updated_at: nil, type: nil, teacher_id: nil, login: nil, e... 
+3.0.0 :002 > user = User.create(email: "ria@gmail.com", login: "Ria")
+Callback used => 
+#<User:0x00007fc520387718   
+...                         
+3.0.0 :003 > user.valid?
  => true 
+
 =end

a19bcf2873df14172a8fbdd16d4968fddef7027b
Mahima-01 <102168844+Mahima-01@users.noreply.github.com>
2022-06-21 17:43:30 +0530

    Practised after_initialize and after_find in callbacks

--- Rails/Library/app/models/user.rb
+++ Rails/Library/app/models/user.rb
@@ -1,39 +1,42 @@
 class User < ApplicationRecord
-  #has_many :students, class_name: "User", foreign_key: "teacher_id"
-  #belongs_to :teacher, class_name: "User", optional:true
-  #validates :login, :email, presence: true
-  #around_create :ensure_login_has_a_value
-  after_commit :send_email_notifications, on: :create
-  
-  def send_email_notifications
-    # This will send email notifications in the background 
-    print "email notification sent."
-  end 
+  after_initialize do |user|
+    puts "You have initialized an object!"
+  end
+
+  after_find do |user|
+    puts "You have found an object!"
+  end
 end
 
 
+
 =begin
-# after_commit: 
-This callback is called after a record has been created, updated, or destroyed.
-You can specify that the callback should only be fired by a certain action with the :on option:
+# after_initialize and after_find:
+The after_initialize callback will be called whenever an Active Record object is instantiated, 
+either by directly using new or when a record is loaded from the database. It can be useful to avoid the need to directly override your Active Record initialize method.
 
-after_commit callback will ensure that the send_email_notifications 
-method is executed only when the commit transaction completes in the database.
+The after_find callback will be called whenever Active Record loads a record from the database. 
+after_find is called before after_initialize if both are defined.
+The after_initialize and after_find callbacks have no before_* counterparts, but they can be registered just like the other Active Record callbacks.
 
-3.0.0 :001 > user = User.new
- => #<User:0x00007ff9e0a67d90 id: nil, first_name: nil, last_name: nil, created_at: nil, updated_at: nil, type: nil, teacher_id: nil, login: nil, e... 
-3.0.0 :002 > user = User.create(email: "ria@gmail.com")
-  TRANSACTION (0.2ms)  BEGIN
-  User Create (0.9ms)  INSERT INTO "users" ("first_name", "last_name", "created_at", "updated_at", "type", "teacher_id", "login", "email") VALUES ($1, $2, $3, $4, $5, $6, $7, $8) RETURNING "id"  [["first_name", nil], ["last_name", nil], ["created_at", "2022-06-15 13:11:20.313029"], ["updated_at", "2022-06-15 13:11:20.313029"], ["type", nil], ["teacher_id", nil], ["login", nil], ["email", "ria@gmail.com"]]
-  TRANSACTION (6.5ms)  COMMIT
-email notification sent. => 
-#<User:0x00007ff9e0387160
+3.0.0 :005 > user = User.new
+You have initialized an object!
+ => #<User:0x00007f0e5c944fc8 id: nil, first_name: nil, last_name: nil, created_at: nil, updated_at: nil, type: nil, teacher_id: nil, login: nil, e... 
 
-3.0.0 :003 > user = User.create(email: "manni@gmail.com")
-  TRANSACTION (0.2ms)  BEGIN
-  User Create (0.3ms)  INSERT INTO "users" ("first_name", "last_name", "created_at", "updated_at", "type", "teacher_id", "login", "email") VALUES ($1, $2, $3, $4, $5, $6, $7, $8) RETURNING "id"  [["first_name", nil], ["last_name", nil], ["created_at", "2022-06-15 13:12:00.397444"], ["updated_at", "2022-06-15 13:12:00.397444"], ["type", nil], ["teacher_id", nil], ["login", nil], ["email", "manni@gmail.com"]]
-  TRANSACTION (2.3ms)  COMMIT
-email notification sent. => 
-#<User:0x0000561fffa7e890
-...
+3.0.0 :007 > User.first
+  User Load (1.2ms)  SELECT "users".* FROM "users" ORDER BY "users"."id" ASC LIMIT $1  [["LIMIT", 1]]
+You have found an object!                                   
+You have initialized an object!                  
+ =>                                              
+#<User:0x00007f0e5c8cde28                        
+ id: 1,                                          
+ first_name: "Mary",                             
+ last_name: nil,                                 
+ created_at: Thu, 09 Jun 2022 13:33:43.427810000 UTC +00:00,
+ updated_at: Thu, 09 Jun 2022 13:33:43.427810000 UTC +00:00,
+ type: nil,                                      
+ teacher_id: nil,                                
+ login: nil,                                     
+ email: nil>                                                      
+
 =end

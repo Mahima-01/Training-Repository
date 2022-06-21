3509afb4125066b7db07b36fd48e5d32b72319c2
Mahima-01 <102168844+Mahima-01@users.noreply.github.com>
2022-06-21 19:11:32 +0530

    Practised Relational Callbacks.

--- Rails/Library/app/models/author.rb
+++ Rails/Library/app/models/author.rb
@@ -1,10 +1,3 @@
 class Author < ApplicationRecord
-  has_many :books
-  has_many :categories
-  after_touch :log_when_books_or_categories
-
-  private
-    def log_when_books_or_categories
-      puts 'Book/Categories was touched'
-    end
+  has_many :books, dependent: :destroy
 end
--- Rails/Library/app/models/book.rb
+++ Rails/Library/app/models/book.rb
@@ -1,32 +1,67 @@
 class Book < ApplicationRecord
-  belongs_to :author, touch: true
-  after_touch do
-    puts 'An author was touched'
+  after_destroy :log_destroy_action
+
+  def log_destroy_action
+    puts 'Article destroyed'
   end
 end
 
 =begin
-#It can be used along with belongs_to:
-3.0.0 :001 > @book = Book.last
-  Book Load (1.3ms)  SELECT "books".* FROM "books" ORDER BY "books"."id" DESC LIMIT $1  [["LIMIT", 1]]
+#  Relational Callbacks
+Callbacks work through model relationships, and can even be defined by them. Suppose an example where a user has many articles. A user's articles should be destroyed if the user is destroyed. Let's add an after_destroy callback to the User model by way of its relationship to the Article model:
+3.0.0 :001 > user = User.first
+  User Load (0.2ms)  SELECT "users".* FROM "users" ORDER BY "users"."id" ASC LIMIT $1  [["LIMIT", 1]]
  =>                                                                
-#<Book:0x000055a072031a98
-
-3.0.0 :003 > @book
+#<User:0x0000564b379d5c28
+3.0.0 :002 > user
  => 
-#<Book:0x000055a072031a98                                          
- id: 24,                                                           
- title: "Programming",            
- author: nil,                   
- created_at: Wed, 08 Jun 2022 09:30:32.962006000 UTC +00:00,
- updated_at: Wed, 08 Jun 2022 09:30:32.962006000 UTC +00:00,
- price: 0.6e3,
- lock_version: 0,
- author_id: 1000052> 
+#<User:0x0000564b379d5c28                                          
+ id: 1,                                                            
+ first_name: "Mary",                                               
+ last_name: nil,                                                   
+ created_at: Thu, 09 Jun 2022 13:33:43.427810000 UTC +00:00,       
+ updated_at: Thu, 09 Jun 2022 13:33:43.427810000 UTC +00:00,       
+ type: nil,                                                        
+ teacher_id: nil,                                                  
+ login: nil,
+ email: nil> 
 
-3.0.0 :004 > @book.touch
+3.0.0 :015 > author.save
+  TRANSACTION (0.3ms)  BEGIN
+  Author Create (9.2ms)  INSERT INTO "authors" ("created_at", "updated_at", "lock_version", "email", "name", "books_count") VALUES ($1, $2, $3, $4, $5, $6) RETURNING "id"  [["created_at", "2022-06-21 13:37:07.430621"], ["updated_at", "2022-06-21 13:37:07.430621"], ["lock_version", 0], ["email", nil], ["name", nil], ["books_count", 0]]                                                     
+  TRANSACTION (0.6ms)  COMMIT                                                      
+ => true  
+
+3.0.0 :016 > author.books.create!
   TRANSACTION (0.1ms)  BEGIN
-  Book Update (0.4ms)  UPDATE "books" SET "updated_at" = $1, "lock_version" = $2 WHERE "books"."id" = $3 AND "books"."lock_version" = $4  [["updated_at", "2022-06-21 13:24:30.419496"], ["lock_version", 1], ["id", 24], ["lock_version", 0]]
-  TRANSACTION (1.2ms)  COMMIT                                
- => true 
+  Book Create (0.6ms)  INSERT INTO "books" ("title", "author", "created_at", "updated_at", "price", "lock_version", "author_id") VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING "id"  [["title", nil], ["author", nil], ["created_at", "2022-06-21 13:37:26.652119"], ["updated_at", "2022-06-21 13:37:26.652119"], ["price", nil], ["lock_version", 0], ["author_id", 1000053]] 
+  TRANSACTION (0.4ms)  COMMIT                                 
+ =>                                                           
+#<Book:0x0000564b3a754758                         
+ id: 25,                                          
+ title: nil,                                      
+ author: nil,                                     
+ created_at: Tue, 21 Jun 2022 13:37:26.652119000 UTC +00:00,
+ updated_at: Tue, 21 Jun 2022 13:37:26.652119000 UTC +00:00,
+ price: nil,                                      
+ lock_version: 0,                                 
+ author_id: 1000053>
+
+3.0.0 :017 > author.destroy
+  TRANSACTION (0.1ms)  BEGIN
+  Book Load (0.2ms)  SELECT "books".* FROM "books" WHERE "books"."author_id" = $1  [["author_id", 1000053]]
+  Book Destroy (0.2ms)  DELETE FROM "books" WHERE "books"."id" = $1 AND "books"."lock_version" = $2  [["id", 25], ["lock_version", 0]]
+Article destroyed                                                                  
+  Author Destroy (0.3ms)  DELETE FROM "authors" WHERE "authors"."id" = $1 AND "authors"."lock_version" = $2  [["id", 1000053], ["lock_version", 0]]
+  TRANSACTION (6.6ms)  COMMIT                                                      
+ =>                                                                                
+#<Author:0x0000564b39df4d20                                                        
+ id: 1000053,                                                                      
+ created_at: Tue, 21 Jun 2022 13:37:07.430621000 UTC +00:00,                       
+ updated_at: Tue, 21 Jun 2022 13:37:07.430621000 UTC +00:00,                       
+ lock_version: 0,                                                                  
+ email: nil,                                                                       
+ name: nil,                                                                        
+ books_count: 0> 
+ 
 =end

4e4b40d3248d2a117cb27c5d38a944937bfb108f
Mahima Tewatia <mahima.tewatia@itsacheckmate.com>
2022-06-23 18:41:34 +0530

    Practised rails redirect and View template.

--- Rails/Library/app/controllers/demo_controller.rb
+++ Rails/Library/app/controllers/demo_controller.rb
@@ -1,11 +1,12 @@
 class DemoController < ApplicationController
   def index
     #render('demo/index')
-    render('hello') # This will return code written in hello,i.e This is hello page.
+    #render('hello') # This will return code written in hello,i.e This is hello page.
   end
 
   def hello
     #render('hello')
-    render('index') # This will return code written in index.
+    #render('index') # This will return code written in index.
+    #redirect_to(:controller=> 'demo', :action=> 'index')
   end
 end

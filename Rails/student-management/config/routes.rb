792430fd51b1888b4d15f2a5f09eaa42543fb151
Mahima Tewatia <mahima.tewatia@itsacheckmate.com>
2022-06-27 18:12:16 +0530

    Practised Member and Collection Routing.

--- Rails/student-management/config/routes.rb
+++ Rails/student-management/config/routes.rb
@@ -3,5 +3,13 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  resources :students
+  resources :students do
+    member do
+      get :personal_details
+    end
+
+    collection do
+      get :active
+    end
+  end
 end

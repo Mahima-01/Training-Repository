7d8a8a0f587308b5afba01daa2d765dece3bd2c7
Mahima Tewatia <mahima.tewatia@itsacheckmate.com>
2022-06-27 15:05:13 +0530

    Created a new rails application student_mgmt and created form for students and practised create, show, edit and update action on it.

---
+++ Rails/student-management/db/seeds.rb
@@ -0,0 +1,20 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
+#   Character.create(name: "Luke", movie: movies.first)
+
+student = Student.create(
+    first_name: 'Mahima', last_name: 'Tewatia', email: 'mahima@gmail.com'
+)
+
+10.times do |i|
+    puts "Creating student #{i+1}"
+    Student.create(
+        first_name: "Student #{i+1}",
+        last_name: "Lname #{i+1}",
+        email: "Student #{i+1}@gmail.com"
+    )
+end

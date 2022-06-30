4f3d8ab38060ef3156d6675d0b500f92ce4bb289
Mahima Tewatia <mahima.tewatia@itsacheckmate.com>
2022-06-30 21:44:39 +0530

    Logging Out Users

--- Rails/scheduled_tweets/config/routes.rb
+++ Rails/scheduled_tweets/config/routes.rb
@@ -6,4 +6,6 @@ Rails.application.routes.draw do
   root to: "main#index"
   get "sign_up", to: "registrations#new"
   post "sign_up", to: "registrations#create"
+  delete "logout", to: "session#destroy"
+  
 end

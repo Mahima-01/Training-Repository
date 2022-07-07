01ad0ac0e5913e567d5cc566fe1ce9d884e88f42
Mahima Tewatia <mahima.tewatia@itsacheckmate.com>
2022-07-07 19:25:45 +0530

    Created api for User

--- Rails/Assignment1/config/routes.rb
+++ Rails/Assignment1/config/routes.rb
@@ -3,9 +3,16 @@ Rails.application.routes.draw do
   root "users#index"
   
   resources :users
+  
+  namespace :api do
+    namespace :v1 do
+      resources :users
+    end
+  end
   # Defines the root path route ("/")
   # root "articles#index"
-  scope module: :admin do
-    resources :users
-  end
+ # scope module: :admin do
+  #  resources :users
+  #end
+
 end

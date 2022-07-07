fb95f03017ff8ddca76798c6327570006e37c764
Mahima Tewatia <mahima.tewatia@itsacheckmate.com>
2022-07-07 16:21:56 +0530

    Created radio, textfield, text, password and select field for user.

--- Rails/Assignment1/app/controllers/users_controller.rb
+++ Rails/Assignment1/app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def index
-    @users = User.all
+    @users = User.all.order(:id)
   end
     
   def new
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
   end
     
   def create
-    @user = User.new(User_params)
+    @user = User.new(user_params)
     if @user.save
       CrudNotificationMailer.create_notification(@user).deliver_now
       redirect_to users_path
@@ -35,7 +35,17 @@ class UsersController < ApplicationController
   end
     
   def destroy
+    @user = User.find(params[:id])
     @user.destroy
-    redirect_to users_in
+    redirect_to users_path
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :gender, :country)
+  end
+
+  def set_user
+    params.require(:user).permit(:first_name, :last_name, :email, :gender, :country)
   end
 end 
--- Rails/Assignment1/config/routes.rb
+++ Rails/Assignment1/config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root "users#index"
   
-  resources :user
+  resources :users
   # Defines the root path route ("/")
   # root "articles#index"
+  scope module: :admin do
+    resources :users
+  end
 end

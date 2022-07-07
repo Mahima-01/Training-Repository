c3582d48227c24fe3dc100f720820910ef3cab20
Mahima Tewatia <mahima.tewatia@itsacheckmate.com>
2022-07-07 14:56:54 +0530

    Created action mailer crudnotificationmailer for user

---
+++ Rails/Assignment1/test/mailers/crud_notification_mailer_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class CrudNotificationMailerTest < ActionMailer::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
---
+++ Rails/Assignment1/test/mailers/previews/crud_notification_mailer_preview.rb
@@ -0,0 +1,4 @@
+# Preview all emails at http://localhost:3000/rails/mailers/crud_notification_mailer
+class CrudNotificationMailerPreview < ActionMailer::Preview
+
+end

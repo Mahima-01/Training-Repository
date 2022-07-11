2bde777dc9216304b06730eedf0ff40235abf71d
Mahima Tewatia <mahima.tewatia@itsacheckmate.com>
2022-07-11 13:47:29 +0530

    Created post request for api

--- Rails/Assignment1/api.rb
+++ Rails/Assignment1/api.rb
@@ -1,7 +1,7 @@
 require 'json'
 require 'rest-client'
 require 'open-uri'
-
+=begin
 url = "https://reqres.in/api/users?page=1"
 response = RestClient.get(url)
 data_h = JSON.parse(response)
@@ -14,3 +14,16 @@ data_h['data'].each do |user|
   @gender = user['gender']
   User.create(email: @email, first_name: @first_name, last_name: @last_name, gender: @gender)
 end
+=end
+
+#https://reqres.in/api/users/2
+url = "https://reqres.in/api/users"
+print url,"\n"
+response1 = RestClient.post url, {
+  "name": "Ria",
+  "job": "Name"
+}
+data_h = JSON.parse(response1)
+url = "https://reqres.in/api/users/#{@id}"
+response2 = RestClient.get url
+print response2.body
--- Rails/Assignment1/app/controllers/api/v1/users_controller.rb
+++ Rails/Assignment1/app/controllers/api/v1/users_controller.rb
@@ -1,50 +1,50 @@
 class Api::V1::UsersController < ApplicationController
-  skip_before_action :verify_authenticity_token
+    skip_before_action :verify_authenticity_token
+    
+    def index
+      users = User.all.order(:id)
+      render json: {data: ActiveModelSerializers::SerializableResource.new(users, each_serializer: UserSerializer)}
+    end
   
-  def index
-    users = User.all.order(:id)
-    render json: {data: ActiveModelSerializers::SerializableResource.new(users, each_serializer: UserSerializer)}
-  end
-
-  def show
-    user = User.find(params[:id])
-    render json: {data: UserSerializer.new(user)}
-  end
-
-  def create
-    @user = User.new(user_params)
-    if @user.save
-      #CrudNotificationMailer.create_notification(@user).deliver_now
-      Sidekiq::Client.enqueue_to_in("default", Time.now + 2.seconds, MailWorker, @user.email, @user.first_name)
-      redirect_to users_path
-    else
-      render :new
-    end
-  end
-
-  def update
-    user = User.find(params[:id])
-    user.update user_params
-    if user.save
+    def show
+      user = User.find(params[:id])
       render json: {data: UserSerializer.new(user)}
-    else
-      render json: {data: user.errors}
     end
-  end
-
-  def destroy
-    user = User.find(params[:id])
-    user.destroy
-    render json: {data: "Successfully deleted  #{user.first_name}"}
-  end
-
-
-  private
-  def user_params
-    params.require(:user).permit(:first_name, :last_name, :email, :gender, :country)
-  end
-
-  def set_user
-    params.require(:user).permit(:first_name, :last_name, :email, :gender, :country)
-  end
+  
+    def create
+      @user = User.new(user_params)
+      if @user.save
+        #CrudNotificationMailer.create_notification(@user).deliver_now
+        Sidekiq::Client.enqueue_to_in("default", Time.now + 2.seconds, MailWorker, @user.email, @user.first_name)
+        redirect_to users_path
+      else
+        render :new
+      end
+    end
+  
+    def update
+      user = User.find(params[:id])
+      user.update user_params
+      if user.save
+        render json: {data: UserSerializer.new(user)}
+      else
+        render json: {data: user.errors}
+      end
+    end
+  
+    def destroy
+      user = User.find(params[:id])
+      user.destroy
+      render json: {data: "Successfully deleted  #{user.first_name}"}
+    end
+  
+  
+    private
+    def user_params
+      params.require(:user).permit(:first_name, :last_name, :email, :gender, :country)
+    end
+  
+    def set_user
+      params.require(:user).permit(:first_name, :last_name, :email, :gender, :country)
+    end
 end

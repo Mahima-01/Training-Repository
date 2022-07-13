2a89e96a5a38aa4777416c561d2b8c239a874e1a
Mahima Tewatia <mahima.tewatia@itsacheckmate.com>
2022-07-13 19:11:56 +0530

    Created views and routes for user

--- Rails/Rails 5.2.0/image_uploader/app/controllers/users_controller.rb
+++ Rails/Rails 5.2.0/image_uploader/app/controllers/users_controller.rb
@@ -2,13 +2,17 @@ class UsersController < ApplicationController
   def index
     @users = User.all
   end
-
+  
   def sign_up
+    @user = User.new
+  end
+
+  def create
     @user = User.new(user_params)
     if @user.save
       redirect_to root_path, notice: "Successfully created user"
     else
-      render :new
+      render :sign_up
     end
   end
 
--- Rails/Rails 5.2.0/image_uploader/config/routes.rb
+++ Rails/Rails 5.2.0/image_uploader/config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root "users/index"
-  post "users/sign_up"
+  root "users#index"
+  get "users/sign_up"
+  post "users/create"
 end

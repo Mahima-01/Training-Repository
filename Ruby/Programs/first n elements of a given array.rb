ae0a84be8d8a327318a9f1d05be5c4ec5e823f0b
Mahima Tewatia <mahima.tewatia@itsacheckmate.com>
2022-07-15 18:35:19 +0530

    Ruby program to iterate over the first n elements of a given array

---
+++ Ruby/Programs/first n elements of a given array.rb
@@ -0,0 +1,16 @@
+# Write a Ruby program to iterate over the first n elements of a given array.
+
+arr1 = ['abcde', 'abdf', 'adeab', 'abdgse', 'bdefa', 'bacdef']
+print "Original array:\n"
+print arr1
+
+arr2 = arr1.first(3)
+print "\nFirst 3 elements:\n"
+print arr2
+
+# Output:
+itsacheckmate@itsacheckmate:~/Html_Project/Training-Repository/Ruby/Programs$ ruby first\ n\ elements\ of\ a\ given\ array.rb 
+Original array:
+["abcde", "abdf", "adeab", "abdgse", "bdefa", "bacdef"]
+First 3 elements:
+["abcde", "abdf", "adeab"]

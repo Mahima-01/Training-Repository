ec71636e77003a95a50c128c37b37e6066585829
Mahima Tewatia <mahima.tewatia@itsacheckmate.com>
2022-07-27 19:50:03 +0530

    Adding new column to students table

--- Rails/Library_mngmt_system/db/schema.rb
+++ Rails/Library_mngmt_system/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_27_155735) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_27_164739) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -66,6 +66,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_27_155735) do
     t.string "gender"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email"
   end
 
   create_table "subcategories", force: :cascade do |t|

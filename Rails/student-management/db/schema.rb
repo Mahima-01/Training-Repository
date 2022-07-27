06bdb0c33ce720279a4c37ac7b72cb467b266b73
Mahima Tewatia <mahima.tewatia@itsacheckmate.com>
2022-07-27 18:40:02 +0530

    Installed devise gem and created user

--- Rails/student-management/db/schema.rb
+++ Rails/student-management/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_29_081344) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_19_163252) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -37,6 +37,16 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_29_081344) do
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
+  create_table "commnts", force: :cascade do |t|
+    t.text "content"
+    t.string "commentable_type"
+    t.integer "commentable_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["commentable_id", "commentable_type"], name: "index_commnts_on_commentable_id_and_commentable_type"
+    t.index ["commentable_type", "commentable_id"], name: "index_commnts_on_commentable"
   end
 
   create_table "students", force: :cascade do |t|

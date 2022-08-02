df5c106d7d2a0413025628bdeb4b89cefecd8846
Mahima Tewatia <mahima.tewatia@itsacheckmate.com>
2022-08-02 16:19:32 +0530

    Using will_paginate gem in fine  model

--- Rails/Library_mngmt_system/app/controllers/fines_controller.rb
+++ Rails/Library_mngmt_system/app/controllers/fines_controller.rb
@@ -2,13 +2,7 @@ class FinesController < ApplicationController
   FINES_PER_PAGE = 2
 
   def index
-    @page = params.fetch(:page, 0).to_i
-    if params[:search]
-      @fines = Fine.where('lower(payment_status) LIKE ?', "%#{params[:search].downcase}%").order(:id).offset(@page * FINES_PER_PAGE).limit(FINES_PER_PAGE)
-    else
-      @fines_count = Fine.all.count
-      @fines = Fine.all.order(:id).offset(@page * FINES_PER_PAGE).limit(FINES_PER_PAGE)
-    end
+    @fine = Fine.all.paginate(page: params[:page], per_page: 3)
   end
             
   def new

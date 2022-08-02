8a61ac6aa0ac44c4765ee73f7d38ea6727f71a64
Mahima Tewatia <mahima.tewatia@itsacheckmate.com>
2022-08-02 16:16:02 +0530

    Using will_paginate gem in book model

--- Rails/Library_mngmt_system/app/controllers/books_controller.rb
+++ Rails/Library_mngmt_system/app/controllers/books_controller.rb
@@ -2,14 +2,7 @@ class BooksController < ApplicationController
   BOOKS_PER_PAGE = 3
 
   def index
-    @page = params.fetch(:page, 0).to_i
-    if params[:search]
-      @books_count = Book.where('lower(title) LIKE ?', "%#{params[:search].downcase}%").count
-      @books = Book.where('lower(title) LIKE ?', "%#{params[:search].downcase}%").order(:id).offset(@page * BOOKS_PER_PAGE).limit(BOOKS_PER_PAGE)
-    else
-      @books_count = Book.all.count
-      @books = Book.all.order(:id).offset(@page * BOOKS_PER_PAGE).limit(BOOKS_PER_PAGE)
-    end
+    @book = Book.all.paginate(page: params[:page], per_page: 3)
   end
           
   def new

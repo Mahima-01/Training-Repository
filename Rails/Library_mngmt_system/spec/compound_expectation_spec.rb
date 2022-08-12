324090ce07898faa23280acea5bea21e3631f4b0
Mahima Tewatia <mahima.tewatia@itsacheckmate.com>
2022-08-12 19:27:23 +0530

    it 'supports multiple matchers' do in rspec

---
+++ Rails/Library_mngmt_system/spec/compound_expectation_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe 25 do
+  it 'can test for multiple matchers' do
+    # expect(subject).to be_odd
+    # expect(subject).to be > 20
+  
+    expect(subject).to be_odd.and be > 20
+  end
+
+  

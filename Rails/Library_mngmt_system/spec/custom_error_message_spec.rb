887cfce2925b7d1b900a6272ec6eb62a771b9c65
Mahima Tewatia <mahima.tewatia@itsacheckmate.com>
2022-08-19 13:00:44 +0530

    Resolving errors in custom_error_message spec in rspec

--- Rails/Library_mngmt_system/spec/custom_error_message_spec.rb
+++ Rails/Library_mngmt_system/spec/custom_error_message_spec.rb
@@ -23,7 +23,6 @@ RSpec.describe Card do
   end
   
   it 'has a custom error message' do
-    card.suit = 'Nonsense'
     comparison = 'Spades'
     expect(card.suit).to eq(comparison), "Hey, I expected #{comparison} but I got #{card.suit} instead!"
   end
